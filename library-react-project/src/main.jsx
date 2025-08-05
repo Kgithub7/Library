e661c8d1de2a7ad009ec0d147fb29076b6b402b3
Kobi <knjoku@hawk.illinoistech.edu>
2025-08-05 01:46:18 -0500

    Implement navigation bar

--- library-react-project/src/main.jsx
+++ library-react-project/src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {library} from "@fortawesome/fontawesome-svg-core"
+import { faBars, faShoppingCart, faTimes } from "@fortawesome/free-solid-svg-icons"
+
+library.add(faBars, faShoppingCart, faTimes)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
